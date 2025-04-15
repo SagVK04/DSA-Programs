3c44542e500c3c7910c19ef58b853b888b92e0be
Sagnik <metiyasagnik692@gmail.com>
2025-04-15 23:58:26 +0530

    Updated with strings day 3

--- Str/src/ASCII.java
+++ Str/src/ASCII.java
@@ -2,5 +2,6 @@ public class ASCII {
     public static void main(String[] args) {
         char ch='a';
         System.out.println((char)(ch+1));
+        System.out.println((char)(ch+2));
     }
 }
