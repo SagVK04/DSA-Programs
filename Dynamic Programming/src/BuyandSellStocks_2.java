de738f1bd6046075b819446b504f333e0c4d5d14
Sagnik <metiyasagnik692@gmail.com>
2025-09-13 15:50:04 +0530

    Dynamic Programming Day 19

--- Dynamic Programming/src/BuyandSellStocks_2.java
+++ Dynamic Programming/src/BuyandSellStocks_2.java
@@ -8,7 +8,7 @@ public class BuyandSellStocks_2 {
         int[][] dp = new int[prices.length][2];
         for(int[] a: dp)
             Arrays.fill(a,-1);
-        return findProfit(prices,prices.length,0,1,dp);
+        return findProfit2(prices,prices.length,1);
     }
     public static int findProfit(int[] prices, int length, int ind, int canBuy,int[][] dp){
         if(ind == length)
@@ -27,5 +27,48 @@ public class BuyandSellStocks_2 {
                     findProfit(prices,length,ind+1,0,dp));
         }
         return dp[ind][canBuy] = profit;
+    }
+    public static int findProfit1(int[] prices, int length,int canBuy){
+        int[][] dp = new int[length+1][2];
+        dp[length][0] = 0; dp[length][1] = 0;
+        for(int ind=length-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                int profit = 0;
+                if(buy==1){
+                    profit = Math.max(
+                            dp[ind+1][0]-prices[ind],
+                            dp[ind+1][1]);
+                }
+                else{
+                    profit = Math.max(
+                            dp[ind+1][1]+prices[ind],
+                            dp[ind+1][0]);
+                }
+                dp[ind][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+    public static int findProfit2(int[] prices, int length,int canBuy){
+        int[] ahead = new int[2];
+        for(int ind=length-1;ind>=0;ind--){
+            int[] cur = new int[2];
+            for(int buy=0;buy<=1;buy++){
+                int profit = 0;
+                if(buy==1){
+                    profit = Math.max(
+                            ahead[0]-prices[ind],
+                            ahead[1]);
+                }
+                else{
+                    profit = Math.max(
+                            ahead[1]+prices[ind],
+                            ahead[0]);
+                }
+                cur[buy] = profit;
+            }
+            ahead = cur;
+        }
+        return ahead[1];
     }
 }
