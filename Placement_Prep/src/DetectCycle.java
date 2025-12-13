e3fddb4a27e383f9b8f40eeab1de2c4700feaca3
Sagnik <metiyasagnik692@gmail.com>
2025-12-13 19:11:23 +0530

    DSE prep started (Day 11)

--- Placement_Prep/src/DetectCycle.java
+++ Placement_Prep/src/DetectCycle.java
@@ -10,4 +10,21 @@ public class DetectCycle {
         }
         return false;
     }
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast==slow){
+                slow = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
 }
